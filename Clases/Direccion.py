628f955a2b687b541c6939f3b6b7e8aebf5ff38d
Sebastián Martínez <sebmartinezse@unal.edu.co>
2024-11-17 12:43:37 -0500

    Correciones

--- Clases/Direccion.py
+++ Clases/Direccion.py
@@ -1,10 +1,10 @@
 class Direccion:
     def __init__(self, calle, ciudad, nomenclatura, edificio, barrio, apto):
         self.__calle = calle
-        self.__ciudad = ciudad
         self.__nomenclatura = nomenclatura
-        self.__edificio = edificio
         self.__barrio = barrio
+        self.__ciudad = ciudad
+        self.__edificio = edificio
         self.__apto = apto
     
     def Direccion(self):
--- __main__.py
+++ __main__.py
@@ -5,3 +5,6 @@ from Clases.Fecha import *
 if __name__ == '__main__':
     f1 = Fecha(10, 5, 2006)
     print(f1)
+
+    d1 = Direccion("Calle 92", "74b-08", "Robledo", "Medellin")
+    print(d1)
