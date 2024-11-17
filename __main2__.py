3f0eadd87e8388b3fe52ae6bc857dd6b735ccc60
Richi <rfuentesa@unal.edu.co>
2024-11-17 14:21:09 -0500

    Finish 2

--- __main2__.py
+++ __main2__.py
@@ -2,13 +2,30 @@ from Clases.Usuario import *
 from Clases.Direccion import *
 from Clases.Fecha import *
 
-nombre = input("Introduce tu nombre: ")
-id_usuario = input("Introduce tu ID: ")
-dia, mes , año = Fecha(input("Introduce tu fecha de nacimiento"))
-ciudad_nacimiento = input("Introduce tu ciudad de nacimiento: ")
-telefono = input("Introduce tu teléfono: ")
-email = input("Introduce tu email: ")
-direccion = Direccion(input("Introduce tu dirreción: "))
-
-Us_new = Usuario(nombre, id_usuario, dia, mes, año, ciudad_nacimiento, telefono, email, direccion)
-print(Us_new)
+#Pedido por consola literal 5
+print("ingrese sus datos: ")
+nombre = input("")
+id = input()
+ciudadNacimiento = input()
+telefono =input()
+email = input("")
+usu1 = Usuario(nombre,id,ciudadNacimiento,telefono,email)
+    
+print("ingrese su fecha de nacimeinto")
+dd = input()
+mm = input()
+aa = input()
+f = Fecha(dd,mm,aa)
+usu1.setFechaNacimiento(f)
+    
+print("ingrese su direccion ")
+calle = input("")
+nomenclatura = input("")
+barrio = input("")
+ciudad = input("")
+edificio = input("")
+apto = input("")
+d = Direccion(calle,nomenclatura,barrio,ciudad,edificio,apto)
+usu1.setDir(d)
+    
+print(nombre +" "+ str(id) +" "+ str(dd) +" - "+ str(mm) +" - "+ str(aa) +" "+ ciudadNacimiento +" "+ str(telefono) +" "+ email +" "+ calle +" #"+ nomenclatura +" "+ barrio +" "+ ciudad + " "+ edificio + " "+ apto)
