8ec84f64fb89a82052d520ffafb198d870480f0f
Sebastián Martínez <sebmartinezse@unal.edu.co>
2024-11-17 10:49:27 -0500

    Creación clase Fecha

---
+++ Fecha.py
@@ -0,0 +1,24 @@
+class Fecha():
+    def __init__(self, dd, mm, aa):
+        self.__dia = dd
+        self.__mes = mm
+        self.__año = aa
+    
+    def get_dia(self):
+        return
+    
+    def set_dia(self, dd):
+        self.__dia = dd
+    
+    def get_mes(self):
+        return
+
+    def set_mes(self, mm):
+        self.__mes = mm
+    
+    def get_año(self):
+        return
+    
+    def set_año(self, aa):
+        self.__año = aa
+    
