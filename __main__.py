c4bbb3437ebc0eedffd2c791e60c8b4351463d97
Sebastián Martínez <sebmartinezse@unal.edu.co>
2024-11-17 12:59:10 -0500

    commit

--- __main__.py
+++ __main__.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     f1 = Fecha(10, 5, 2006)
     print(f1)
 
-    d1 = Direccion("Calle 92 ", " 74b-08 ", " Robledo ", " Medellin ", " Altos de Robledo ", " 608")
+    d1 = Direccion("Calle 92 ", "74b-08 ", " Robledo ", " Medellin ", " Altos de Robledo ", " 608")
     print(d1)
